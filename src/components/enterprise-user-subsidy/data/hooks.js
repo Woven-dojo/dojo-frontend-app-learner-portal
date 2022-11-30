b3a50f897f7e70de3f02fd1f9ccbc0e3d1add150
tetiana1506 <tetiana.brzezinska@woven-planet.global>
2022-11-30 11:02:13 +0100

    DSC-231 fix locale for applyFilter hook

--- src/components/enterprise-user-subsidy/data/hooks.js
+++ src/components/enterprise-user-subsidy/data/hooks.js
@@ -9,7 +9,7 @@ import { fetchEnterpriseCatalogData, fetchLearningPathData, requestCourse } from
  * triggering a new query (via filters) will result in the UserSubsidyContext.Provider
  * returning a loading screen.
  */
-const applyFilter = (courses = [], filter = {}) => {
+const applyFilter = (courses = [], filter = {}, locales = []) => {
   let filteredCourses = [...courses];
   if (filter.learningPaths.length) {
     filteredCourses = filteredCourses.filter((course) =>
@@ -31,7 +31,7 @@ const applyFilter = (courses = [], filter = {}) => {
 
   if (filter.search.length) {
     filteredCourses = filteredCourses.filter((course) =>
-      course.full_description.toLocaleLowerCase().includes(filter.search.toLocaleLowerCase()),
+      course.full_description.toLocaleLowerCase(locales).includes(filter.search.toLocaleLowerCase(locales)),
     );
   }
 
